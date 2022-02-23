8de7396601e02c073cdf7e082edc632749e1ed24
Matt Payne <matthewjohnpayne@gmail.com>
2022-02-22 19:25:54 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ import setuptools
 setuptools.setup(
     name="mpc_orb_creation",
     version="0.0.1",
-    install_requires=['jsonschema', 'json'],
+    install_requires=['jsonschema'],
     packages=setuptools.find_packages()
 )
