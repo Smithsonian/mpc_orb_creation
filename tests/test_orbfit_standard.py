9dbb3930756a451bf1838f12b9d1d0dcd90f7a27
matthewjohnpayne <mpayne@cfa.harvard.edu>
2022-07-14 14:58:19 +0000

    developing tests for orbfit_standard input

---
+++ tests/test_orbfit_standard.py
@@ -0,0 +1,32 @@
+# standard imports
+import os, sys
+
+# local imports
+from mpc_orb_creation import template
+from mpc_orb_creation import schema
+from mpc_orb_creation import construct
+from mpc_orb_creation.filepaths import filepath_dict
+
+# ---- Tests ----
+def test_read():
+  ''' '''
+  # get filepath(s) of sample standardized orbfit output dict/json 
+  fps = filepath_dict['test_pass_orbfit_standard']
+  assert fps
+
+  # loop through it/them ...
+  for fp in fps : 
+    assert os.path.isfile(fp) 
+
+
+def test_construct_A():
+  ''' ''' 
+  # loop through filepath(s) of sample standardized orbfit output dict/json 
+  for fp in filepath_dict['test_pass_orbfit_standard']: 
+    print(fp)
+    
+    # call the construct module...
+    result = construct.construct(fp)
+    print(result)
+if __name__ == '__main__':
+  test_construct_A() 
