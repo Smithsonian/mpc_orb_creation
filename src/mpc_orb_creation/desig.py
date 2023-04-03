c2ad993e4471e9ce5de7cf2a19ad6c244eaec5ad
Chris Moriarty <cmoriarty@cfa.harvard.edu>
2023-04-03 14:03:00 +0000

    desig.py chanegs

--- src/mpc_orb_creation/desig.py
+++ src/mpc_orb_creation/desig.py
@@ -2,7 +2,10 @@
 import requests
 
 r = requests.post('http://mpcweb1:8001/api/query-identifier', data='1983 LM')
+<<<<<<< HEAD
 print('r=',r)
 print()
+=======
+>>>>>>> 6125af168285bb8c75ddcf3770935676161b8216
 print(f"Result from Flask endpoint for /api/query-identifier: {r.json()}")
 
